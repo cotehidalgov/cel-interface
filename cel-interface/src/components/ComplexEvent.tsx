a6e745f942e70bf8bd2dfed7608336da3112f5a7
Maria Jose Hidalgo <mjhidalgo@uc.cl>
2018-11-14 09:50:35 -0300

    events for complex events

--- cel-interface/src/components/ComplexEvent.tsx
+++ cel-interface/src/components/ComplexEvent.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { ComplexEventContainerProps } from "./ComplexEventContainer"
+import { Label, ListGroup, ListGroupItem } from "react-bootstrap"
 
 export interface ComplexEventProps {
   value: string
@@ -8,8 +9,6 @@ export interface ComplexEventProps {
 }
 
 export interface ComplexEventState {
-  // eventsId: number[]
-  // data: { id: number; name: string; value: number; date: Date }[]
   events: { id: number; name: string; value: number; date: Date }[]
 }
 
@@ -20,6 +19,20 @@ class ComplexEvent extends React.Component<
   events = this.getData()
   state = { events: this.events }
 
+  getValueDate(date1: Date, date2: Date) {
+    if (date1.getUTCFullYear() < date2.getUTCFullYear()) return -1
+    else if (date1.getUTCFullYear() > date2.getUTCFullYear()) return 1
+    else {
+      if (date1.getUTCMonth() < date2.getUTCMonth()) return -1
+      else if (date1.getUTCMonth() > date2.getUTCMonth()) return 1
+      else {
+        if (date1.getUTCDate() < date2.getUTCDate()) return -1
+        else if (date1.getUTCDate() > date2.getUTCDate()) return 1
+        else return 0
+      }
+    }
+  }
+
   getData() {
     let events = []
     let event
@@ -27,35 +40,41 @@ class ComplexEvent extends React.Component<
       event = this.props.data[this.props.eventsId[index]]
       events.push(event)
     }
-    events.sort(
-      (e1, e2) =>
-        e1.date.getDate() < e2.date.getDate()
-          ? -1
-          : e1.date.getDate() > e2.date.getDate()
-            ? 1
-            : 0,
-    )
+    events.sort((e1, e2) => this.getValueDate(e1.date, e2.date))
     return events
   }
+
+  getStringDate = (date: Date) => {
+    return (
+      date.getUTCDate().toString() +
+      "/" +
+      date.getUTCMonth().toString() +
+      "/" +
+      date.getUTCFullYear().toString()
+    )
+  }
+
+  // Month: sometimes is 0
 
   render() {
     return (
       <div>
         <p>Complex event {this.props.value}</p>
-        <p>Events:</p>
-        {this.state.events.map(event => (
-          <div
-          // style={{
-          //   display: "inline-flex",
-          //   flexDirection: "row-reverse",
-          // }}
-          >
-            <h5>Event</h5>
-            <p>Event name: {event.name}</p>
-            <p>Event value: {event.value}</p>
-            <p>Event date: {event.date.getDay()}</p>
-          </div>
-        ))}
+        <h4>
+          <Label bsStyle="default">Events</Label>
+        </h4>
+        <div className="pre-x-scrollable" style={{ overflow: "auto" }} />
+        <ListGroup style={{ display: "inline-flex" }}>
+          {this.state.events.map(event => (
+            <ListGroupItem style={{ display: "inline-block", margin: "5px" }}>
+              <h5>
+                <Label bsStyle="default">{event.name}</Label>
+              </h5>
+              <p>Value: {event.value}</p>
+              <p>Date: {this.getStringDate(event.date)}</p>
+            </ListGroupItem>
+          ))}
+        </ListGroup>
       </div>
     )
   }
